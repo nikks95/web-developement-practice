26d2ee679cba24a94233962cf00d17b7728fe8d5
iamnikhil21 <nikhilgola21@gmail.com>
2021-07-10 09:10:25 +0530

    Added calculator functionality

--- Calculator-JS/js/calculator.js
+++ Calculator-JS/js/calculator.js
@@ -17,7 +17,7 @@ let clear = () => {
 let add = (a, b) => a + b;
 let sub = (a, b) => a - b;
 let mul = (a, b) => a * b;
-let div = (a, b) => a / b;
+let div = (a, b) => { if (b == 0) { throw "Division by zero"; } else return a / b; };
 let pow = (a, b) => Math.pow(a, b);
 let checkop = (ch) => {
     switch (ch) {
@@ -30,11 +30,32 @@ let checkop = (ch) => {
         default: return false;
     }
 }
-let checknum = (ch) => {
-    if ((ch >= '0' && ch <= '9') || ch == '.') {
+let evalExp = (opr1, opr2, op) => {
+    let opf = getop(op);
+    let result = opf(parseFloat(opr1), parseFloat(opr2));
+    return result;
+}
+let checknum = (ch, f = true) => {
+
+    if ((ch >= '0' && ch <= '9')) {
+
+        if (f == true && ch == '.')
+            return true;
+        else if (f == false && ch == '.')
+            return false;
         return true;
     } else {
         return false;
+    }
+}
+let opPrecedence = (ch) => {
+    switch (ch) {
+        case '+': return 1;
+        case '-': return 1;
+        case 'x': return 3;
+        case '/': return 4;
+        case '^': return 5;
+        default: return null;
     }
 }
 let getop = (ch) => {
@@ -54,8 +75,8 @@ let splitify = (exp) => {
     for (let i = 0; i < len; i++) {
         if (checkop(exp.charAt(i))) {
             let s = "";
-            
-            for (let j = start; j < i ; j++) {
+
+            for (let j = start; j < i; j++) {
                 s += exp.charAt(j);
             }
             arr.push(s);
@@ -64,10 +85,96 @@ let splitify = (exp) => {
         }
     }
     let s = "";
-    for (let j = start; j < len ; j++) {
+    for (let j = start; j < len; j++) {
         s += "" + exp.charAt(j);
     }
     arr.push(s);
     return arr;
 }
+function validNumber(x) {
+    if ((x.split(".")).length > 2 || !checknum((x.split("."))[0], false)) {
+        return false;
+    }
+    return true;
+}
+function validateInput(expression) {
+    for (let i = 0; i < expression.length; i++) {
+        if (checkop(expression[i])) {
+            if (i == 0) throw "Invalid Expression, expression does not starts with operator";
+            else {
+                if (checkop(expression[i - 1])) {
+                    throw "Invalid Expression, Operator does not follow operator";
+                }
+            }
+        }
+        else {
+            if (!validNumber(expression[i])) throw "Invalid Number";
+        }
+    }
+}
+let getTop = (arr) => arr[arr.length - 1];
+function evaluateExpression(expression) {
+    var operandStack = []
+    var operatorStack = []
 
+    let len = expression.length;
+    try {
+        for (let i = 0; i < len; i++) {
+            if (!checkop(expression[i])) {
+                operandStack.push(parseFloat(expression[i]));
+                
+            }
+            else {
+                if (operatorStack.length > 0) {
+                    while (operatorStack.length > 0 && (opPrecedence(getTop(operatorStack)) > opPrecedence(expression[i]))) {
+                        let opr1, opr2;
+                        opr2 = operandStack.pop();
+                        opr1 = operandStack.pop();
+                        op = operatorStack.pop();
+                        let result = evalExp(opr1, opr2, op);
+                        operandStack.push(result);
+                        
+                    }
+                    operatorStack.push(expression[i]);
+                   
+                }
+                else {
+                    operatorStack.push(expression[i]);
+                   
+                }
+            }
+        }
+        while (operatorStack.length > 0) {
+            let opr1, opr2;
+            opr2 = operandStack.pop();
+            opr1 = operandStack.pop();
+            op = operatorStack.pop();
+            let result = evalExp(opr1, opr2, op);
+            operandStack.push(result);
+        }
+    }
+    catch (e) {
+         alert(e + ": Invalid operation");
+         return "";
+    }
+    return operandStack[0];
+}
+function calculate() {
+    let inp = inputbox.value;
+    if (inp.length > 0) {
+        let expression = splitify(inp);
+        let result = "";
+        try {
+            validateInput(expression);
+            result = evaluateExpression(expression);
+        }
+        catch (e) {
+         alert("" + e);
+        }
+        inputbox.value = result;
+    }
+}
+let clearButton = document.getElementById("clear");
+let equalButton = document.getElementById("equal");
+equalButton.addEventListener("click", () => calculate());
+clearButton.addEventListener("click", () => clear());
